dd4a5461e489d6c84fc566f894dfcd3e3168bb36
mac <mac@macui-MacBookPro.local>
2026-04-21 14:35:37 +0900

    feat: 배열 -> 리스트로 변경 요구사항 8단계까지.

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,87 +1,96 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+class Quote {
+    int id;
+    String content;
+    String author;
+
+    Quote(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
 
         Scanner scanner = new Scanner(System.in);
-        int num = 0;
-        int[] id = new int[100];
-        String[] quotes = new String[100];
-        String[] authors = new String[100];
+        List<Quote> quotes = new ArrayList<>();
+
+        int lastId = 0;
 
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
 
-            if (cmd.equals("종료")) break;
+            if (cmd.equals("종료"))
+                break;
             else if (cmd.equals("등록")) {
-
                 System.out.print("명언 : ");
-                String quote = scanner.nextLine();
+                String content = scanner.nextLine();
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
 
-                id[num] = num + 1;
-                quotes[num] = quote;
-                authors[num] = author;
-                System.out.println(id[num] + "번 명언이 등록되었습니다.");
-                num++;
+                int id = ++lastId;
+                Quote quote = new Quote(id, content, author);
+                quotes.add(quote);
+                System.out.println(id + "번 명언이 등록되었습니다.");
 
             } else if (cmd.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언\n------------------");
-                for (int i = num - 1; i >= 0; i--) {
-                    if (id[i] == 0) continue;
-                    System.out.println(id[i] + " / " + authors[i] + " / " + quotes[i]);
+                System.out.println("번호 / 작가 / 명언\n----------------------");
+                for (int i = quotes.size() - 1; i >= 0; i--) {
+                    Quote quote = quotes.get(i);
+                    System.out.println(quote.id + " / " + quote.author + " / " + quote.content);
                 }
             } else if (cmd.startsWith("삭제?id=")) {
                 String[] parts = cmd.split("=");
                 int deleteId = Integer.parseInt(parts[1]);
 
-                boolean isFound = false;
+                Quote found = null;
 
-                for (int i = 0; i < num; i++) {
-                    if (id[i] == deleteId) {
-                        id[i] = 0;
-                        quotes[i] = null;
-                        authors[i] = null;
-                        isFound = true;
+                for (Quote wiseSaying : quotes) {
+                    if (wiseSaying.id == deleteId) {
+                        found = wiseSaying;
                         break;
                     }
                 }
-                if (isFound) {
-                    System.out.println(deleteId + "번 명언이 삭제되었습니다.");
-                } else {
+
+                if (found == null) {
                     System.out.println(deleteId + "번 명언은 존재하지 않습니다.");
+                } else {
+                    quotes.remove(found);
+                    System.out.println(deleteId + "번 명언이 삭제되었습니다.");
                 }
+
             } else if (cmd.startsWith("수정?id=")) {
                 String[] parts = cmd.split("=");
                 int editId = Integer.parseInt(parts[1]);
 
-                boolean isFound = false;
+                Quote found = null;
 
-                for (int i = 0; i < num; i++) {
-                    if (id[i] == editId) {
-                        System.out.println("명언(기존) : " + quotes[i]);
-                        System.out.print("명언 : ");
-                        String quote = scanner.nextLine();
-
-                        System.out.println("작가(기존) : " + authors[i]);
-                        System.out.print("작가 : ");
-                        String author = scanner.nextLine();
-
-                        quotes[i] = quote;
-                        authors[i] = author;
-
-                        isFound = true;
+                for (Quote wiseSaying : quotes) {
+                    if (wiseSaying.id == editId) {
+                        found = wiseSaying;
                         break;
                     }
                 }
 
-                if (!isFound) {
+                if (found == null) {
                     System.out.println(editId + "번 명언은 존재하지 않습니다.");
+                } else {
+                    System.out.println("명언(기존) : " + found.content);
+                    System.out.print("명언 : ");
+                    found.content = scanner.nextLine();
+
+                    System.out.println("작가(기존) : " + found.author);
+                    System.out.print("작가 : ");
+                    found.author = scanner.nextLine();
                 }
             }
 
